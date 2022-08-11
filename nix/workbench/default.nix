9eeab5cebf3f1b5d6b7cea8f557d9945c4050a4c
Federico Mastellone <federico.pascual-mastellone@iohk.io>
2022-08-11 14:46:06 +0000

    Rename

--- nix/workbench/default.nix
+++ nix/workbench/default.nix
@@ -26,8 +26,10 @@ let
       '';
 
       installPhase = ''
-        mkdir -p         $out/bin
-        cp -a wb chain-filters profiles *.sh *.jq $out/bin
+        mkdir -p                                     $out/bin
+        cp    -a wb chain-filters profiles *.sh *.jq $out/bin
+        mkdir -p                                     $out/bin/backend
+        cp    -a backend/*.sh                        $out/bin/backend
       '';
 
       dontStrip = true;
